66acc8a71570ddda5b29b6f697e741f37ddfe9e9
Milo1389 <petar.milu@hotmail.de>
2021-01-09 17:02:29 +0100

    Aufgabe2

---
+++ src/Aufg2/OneDay.java
@@ -0,0 +1,43 @@
+package Aufg2;
+
+public class OneDay {
+
+    //Attribute
+    private int tag;
+    private int jahr;
+
+    private String monat;
+
+    //Getter und Setter
+    public int getTag() {
+        return tag;
+    }
+
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    public int getJahr() {
+        return jahr;
+    }
+
+    public void setJahr(int jahr) {
+        this.jahr = jahr;
+    }
+
+    public String getMonat() {
+        return monat;
+    }
+
+    public void setMonat(String monat) {
+        this.monat = monat;
+    }
+        //Konstrukor
+    public OneDay(int tag, int jahr, String monat) {
+        this.tag = tag;
+        this.jahr = jahr;
+        this.monat = monat;
+
+
+    }
+}
---
+++ src/Aufg2/OneDayTest.java
@@ -0,0 +1,13 @@
+package Aufg2;
+
+public class OneDayTest {
+    public static void main(String[] args) {
+        
+        OneDay geburtstag = new OneDay(24, 1996, "September");
+        OneDay weihnachten = new OneDay(24,2021, "Dezember");
+
+        System.out.println();
+
+    }
+
+}
