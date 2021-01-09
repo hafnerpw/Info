7419c964ae7a15ad31815881a8027e83a41de08b
Milo1389 <petar.milu@hotmail.de>
2021-01-09 15:36:09 +0100

    hallo test

--- src/Aufg1/HemdTest.java
+++ src/Aufg1/HemdTest.java
@@ -4,7 +4,15 @@ public class HemdTest {
 
     public static void main(String[] args) {
 
+        Hemd gucciHemd = new Hemd();
+            gucciHemd.setHemdId(512);
+            gucciHemd.setBeschreibung("Gucci");
+            gucciHemd.setFarbCode('b');
+            gucciHemd.setPreis(800.00);
+            gucciHemd.setMengeAufLager(50);
 
+        System.out.println("Das Hemd ist von der Marke " + gucciHemd.getBeschreibung() + " in der Farbe " + gucciHemd.getFarbCode()
+        + " und hat einen Preis von " + gucciHemd.getPreis() + " Euro.");
 
     }
 }
