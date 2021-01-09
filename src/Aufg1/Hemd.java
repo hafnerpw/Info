9714907405a38b4b4f0e6f9e4df29382ec5f9ac9
w-hafner <whafner0@gmail.com>
2021-01-09 14:32:13 +0100

    Initial commit

---
+++ src/Aufg1/Hemd.java
@@ -0,0 +1,15 @@
+package Aufg1;
+
+public class Hemd {
+
+    private final int id;
+
+    public Hemd(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ID - " + this.id;
+    }
+}
---
+++ src/Aufg1/Main.java
@@ -0,0 +1,8 @@
+package Aufg1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hemd h1 = new Hemd(121);
+    }
+}
