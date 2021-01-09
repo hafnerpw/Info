cfead70410d00a0907d641a35da649a06f45971c
Milo1389 <petar.milu@hotmail.de>
2021-01-09 23:37:26 +0100

    Aufgabe2

--- src/Aufg3/Kunde.java
+++ src/Aufg3/Kunde.java
@@ -5,13 +5,21 @@ public class Kunde {
     String name;
     String mailAdr;
 
+    public boolean isEqual(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Kunde kunde = (Kunde) o;
+        return mailAdr.equals(kunde.mailAdr);
+    }
+
+
     public Kunde(int id, String name, String mailAdr) {
         this.id = id;
         this.name = name;
         this.mailAdr = mailAdr;
     }
 
-    void displayKunde(){
+    void displayKunde() {
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
         System.out.println("E-Mail: " + mailAdr);
--- src/Aufg3/KundeTest.java
+++ src/Aufg3/KundeTest.java
@@ -3,8 +3,29 @@ package Aufg3;
 public class KundeTest {
 
     public static void main(String[] args) {
-        Kunde k1= new Kunde(1, "Fred", "fred@fred.at");
-        Kunde k2= new Kunde(1, "Fred", "fred@fred.at");
+        Kunde k1 = new Kunde(1, "Fred", "fred@fred.at");
+        Kunde k2 = new Kunde(1, "Fred", "fred@fred.at");
+
         System.out.println(k1.equals(k2));
+        System.out.println(k1.isEqual(k2));
+
+
+        isEquals(k1, k2);
+    }
+
+
+    public static boolean isEquals(Kunde kunde1, Kunde kunde2) {
+
+        return kunde1.mailAdr.equals(kunde2.mailAdr);
     }
 }
+
+
+
+
+
+
+
+
+
+
