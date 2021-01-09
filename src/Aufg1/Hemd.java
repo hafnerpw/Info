4e4e69ae4dac959a1bdabe21308093b89fc64d94
Milo1389 <petar.milu@hotmail.de>
2021-01-09 15:22:34 +0100

    hallo test

--- src/Aufg1/Hemd.java
+++ src/Aufg1/Hemd.java
@@ -8,4 +8,41 @@ public class Hemd {
     private double preis;
     private int mengeAufLager;
 
+
+
+    public void setHemdId(int hemdId) {
+        this.hemdId = hemdId;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public void setBeschreibung(String beschreibung) {
+        this.beschreibung = beschreibung;
+    }
+
+    public char getFarbCode() {
+        return farbCode;
+    }
+
+    public void setFarbCode(char farbCode) {
+        this.farbCode = farbCode;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public void setPreis(double preis) {
+        this.preis = preis;
+    }
+
+    public int getMengeAufLager() {
+        return mengeAufLager;
+    }
+
+    public void setMengeAufLager(int mengeAufLager) {
+        this.mengeAufLager = mengeAufLager;
+    }
 }
