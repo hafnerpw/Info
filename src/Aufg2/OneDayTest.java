f869402cacf90593ba773299a81b61af092d2f78
Milo1389 <petar.milu@hotmail.de>
2021-01-09 17:18:53 +0100

    Aufgabe2

--- src/Aufg2/OneDayTest.java
+++ src/Aufg2/OneDayTest.java
@@ -6,7 +6,16 @@ public class OneDayTest {
         OneDay geburtstag = new OneDay(24, 1996, "September");
         OneDay weihnachten = new OneDay(24,2021, "Dezember");
 
-        System.out.println();
+        OneDay waltersBday = new OneDay();
+            waltersBday.setTag(10);
+            waltersBday.setMonat("Mai");
+            waltersBday.setJahr(1997);
+        System.out.println("Walters Bday ist am:" + waltersBday.getTag() + waltersBday.getMonat() + waltersBday.getJahr());
+
+
+
+
+
 
     }
 
