0228939f3a249d06710d2ccc1a356dd72a5c7a82
Milo1389 <petar.milu@hotmail.de>
2021-01-14 23:28:34 +0100

    Meilenstein3

---
+++ src/Meilenstein3/StudentTest.java
@@ -0,0 +1,16 @@
+package Meilenstein3;
+
+
+
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Albert", "Einstein", 29, "IBIS", new StudienBG("WiSe", 2021), 123456, 2.2);
+
+        student1.displayInformation();
+
+        System.out.println("hi");
+
+    }
+}
